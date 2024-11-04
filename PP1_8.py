ecea5291c2cf2da04855790edefdbcc9d93b90c1
ZhuAnson-ICD201-01 <anson.zhu@student.tdsb.on.ca>
2024-11-04 09:53:05 -0500

    Update PP1_8.py

--- PP1_8.py
+++ PP1_8.py
@@ -1,18 +1,31 @@
 
 def q1():
-  #Write Assignment code here
+  bool1 = True
+  bool2 = False
+  print (bool1 and bool2)
+  print (bool or bool2)
+  
 
 def q2():
-  #Write Assignment code here
+  bool1 = int(input("Enter an integer: "))
+  print (not(not(bool1)))
 
 def q3():
-  #Write Assignment code here
+  bool1 = float(input("Enter a number: "))
+  print (bool1 >= 0 and bool1 <= 10)
 
 def q4():
-  #Write Assignment code here
+  food = input("Input food: ")
+  drink = input("Input drink: ")
+  if food == "pizza" and drink == "pop":
+    print(False
+  else:
+    print(True)
+    
 
 def q5():
-  #Write Assignment code here
+  num = int(input("Enter an integer: "))
+  print(f"The integer {num} is {num % 2 == 0}.")
 
 #Do not edit code after this
 #Comment out the followwing code when running tests
